84ef9df9ad236bed3a17cd18dcef8868d13faacd
Thiago Souza <tsouzatm@outlook.com>
2024-01-09 14:38:30 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double Salary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double salary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            Salary = salary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sales)
+        {
+            Sales.Add(sales);
+        }
+
+        public void RemoveSales(SalesRecord sales)
+        {
+            Sales.Remove(sales);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(x => x.Date >= initial && x.Date <= final).Sum(x => x.Amount);
+        }
+    }
+}
