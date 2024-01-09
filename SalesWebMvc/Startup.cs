8e31996eb40910d9d98dd73a4030521e4cdb16b8
Thiago Souza <tsouzatm@outlook.com>
2024-01-09 12:32:44 -0300

    Crud Scaffoling

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -9,6 +9,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
+
 
 namespace SalesWebMvc
 {
@@ -33,6 +36,9 @@ namespace SalesWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
